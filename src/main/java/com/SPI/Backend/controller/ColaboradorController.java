20d5fdd3e367ec615f6d98da69933973cdbef777
CarlosPossi <rm558758@fiap.com.br>
2026-05-07 23:00:19 -0300

    feat: criação do controller de Colaborador

--- src/main/java/com/SPI/Backend/controller/ColaboradorController.java
+++ src/main/java/com/SPI/Backend/controller/ColaboradorController.java
@@ -1,4 +1,44 @@
 package com.SPI.Backend.controller;
+import com.SPI.Backend.model.Colaborador;
+import com.SPI.Backend.model.SessaoTrabalho;
+import com.SPI.Backend.model.Postura;
+import com.SPI.Backend.service.ColaboradorService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/colaboradores")
+@CrossOrigin
 
 public class ColaboradorController {
+    private final ColaboradorService service;
+
+    public ColaboradorController(ColaboradorService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public Colaborador criar(@RequestBody Colaborador colaborador){
+        return service.salvar(colaborador);
+    }
+
+    @GetMapping
+    public List<Colaborador> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Colaborador buscarPorId(@PathVariable long id){
+        return service.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public Colaborador atualizar(@PathVariable Long id, @RequestBody Colaborador colaborador){
+        return service.atualizar(id, colaborador);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){
+        service.deletar(id);
+    }
 }
