5d1cdcfb51e08b23857027a02f4d2bd33c250188
CarlosPossi <rm558758@fiap.com.br>
2026-05-07 23:14:26 -0300

    feat: criação do repository de Postura

---
+++ src/main/java/com/SPI/Backend/repository/PosturaRepository.java
@@ -0,0 +1,6 @@
+package com.SPI.Backend.repository;
+import com.SPI.Backend.model.Postura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PosturaRepository extends JpaRepository<Postura, Long>{
+}
