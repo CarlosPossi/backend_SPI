2c32aab996126df2dd4e95c7341cfd71b3213a7b
CarlosPossi <rm558758@fiap.com.br>
2026-05-07 23:10:55 -0300

    feat: criação do repository de IndicadorSeguranca

---
+++ src/main/java/com/SPI/Backend/repository/IndicadorSegurancaRepository.java
@@ -0,0 +1,6 @@
+package com.SPI.Backend.repository;
+import com.SPI.Backend.model.IndicadorSeguranca;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IndicadorSegurancaRepository extends JpaRepository<IndicadorSeguranca, Long> {
+}
