a714b8c5311b8b3053ea7d27814a108f704ea3ff
CarlosPossi <rm558758@fiap.com.br>
2026-05-07 22:58:18 -0300

    feat: criação do repository de Colaborador

--- src/main/java/com/SPI/Backend/repository/ColaboradorRepository.java
+++ src/main/java/com/SPI/Backend/repository/ColaboradorRepository.java
@@ -1,4 +1,6 @@
 package com.SPI.Backend.repository;
+import com.SPI.Backend.model.Colaborador;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class ColaboradorRepository {
+public interface ColaboradorRepository extends JpaRepository<Colaborador, Long> {
 }
