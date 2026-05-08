37ce8002b69f59f62f8365aeb4cb5e01e08bf927
CarlosPossi <rm558758@fiap.com.br>
2026-05-07 23:02:03 -0300

    chore: configuração do banco H2

--- src/main/java/com/SPI/Backend/BackendApplication.java
+++ src/main/java/com/SPI/Backend/BackendApplication.java
@@ -1,7 +1,7 @@
 package com.SPI.Backend;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class BackendApplication {
