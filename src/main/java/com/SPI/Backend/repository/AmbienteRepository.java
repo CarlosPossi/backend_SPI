1b53275fc695a215c7dd114c1791fd1220b04b5b
CarlosPossi <rm558758@fiap.com.br>
2026-05-07 23:06:32 -0300

    feat: criação do repository de Ambiente

---
+++ src/main/java/com/SPI/Backend/repository/AmbienteRepository.java
@@ -0,0 +1,6 @@
+package com.SPI.Backend.repository;
+import com.SPI.Backend.model.Ambiente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AmbienteRepository extends JpaRepository<Ambiente, Long>{
+}
