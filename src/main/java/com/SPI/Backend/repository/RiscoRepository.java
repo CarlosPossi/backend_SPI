5455ca5b0d68cdb2a73a7bf145210dd13e511c27
CarlosPossi <rm558758@fiap.com.br>
2026-05-07 23:19:57 -0300

    fix: Arrumando o repository de Risco

--- src/main/java/com/SPI/Backend/repository/RiscoRepository.java
+++ src/main/java/com/SPI/Backend/repository/RiscoRepository.java
@@ -1,4 +1,7 @@
 package com.SPI.Backend.repository;
+import com.SPI.Backend.model.Risco;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class RiscoRepository {
+
+public interface RiscoRepository extends JpaRepository<Risco, Long> {
 }
